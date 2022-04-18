9535fcde1f11497798a126228f547d6206fbd38d
vixxer <83184947+denchikwot333@users.noreply.github.com>
2022-04-18 16:39:05 +0300

    Create Settings.cs

---
+++ Audiroriesinfo.editor/Audiroriesinfo.editor/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Common.ConsoleIO
+{
+    static class Settings
+    {
+        public static void SetConsoleParam()
+        {         
+            Console.Clear();
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+        }
+
+    }
+}
