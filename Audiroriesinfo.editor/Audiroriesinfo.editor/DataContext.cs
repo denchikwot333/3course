1c1e9e6d5e7ce8550ebe96dec3771ee3e2476326
vixxer <83184947+denchikwot333@users.noreply.github.com>
2022-04-18 16:39:14 +0300

    Create DataContext.cs

---
+++ Audiroriesinfo.editor/Audiroriesinfo.editor/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+namespace AuditoriInfo
+{
+    class DataContext
+    {
+        public static readonly ArrayList objectsList = new ArrayList();
+
+        public static void CreateTestingData()
+        {
+            objectsList.AddRange(objectsArray);
+        }
+
+        static Auditori[] objectsArray = {
+            new Auditori(){
+                name = "ІТ",
+                number = 424,
+                note =" в наявності 16 ноутбуків, інтерактивна дошка але відсутній головний компьютер для показу демонстарції екрану "
+            },
+            new Auditori(){
+                name = "Хімічна лабораторія",
+                number = 12,
+                note = ""
+            },
+        };
+    }
+}
